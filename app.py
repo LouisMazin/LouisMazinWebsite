d2d61cd035bbdf51474ecba6690edc76746ba556
Louis Mazin <mazinlouis79@gmail.com>
2024-10-15 16:31:03 +0200

    test

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from nicegui import ui
 ui.label('Hello NiceGUI!')
 ui.button('BUTTON', on_click=lambda: ui.notify('button was pressed'))
 
-if __name__ == '__main__':
+if __name__ in {"__main__", "__mp_main__"}:
     ui.run(host="0.0.0.0", port=1072)
