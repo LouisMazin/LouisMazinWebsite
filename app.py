19917fa866fd2002b9174b5aad21a3578842ad6a
Louis Mazin <mazinlouis79@gmail.com>
2024-10-15 16:33:04 +0200

    test menu

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ from nicegui import ui
 
 ui.label('Hello NiceGUI!')
 ui.button('BUTTON', on_click=lambda: ui.notify('button was pressed'))
+ui.menu('MENU', ['one', 'two', 'three'], on_change=lambda value: ui.notify(f'menu changed to {value}'))
+
+
+
+
+
+
+
+
+
+
 
 if __name__ in {"__main__", "__mp_main__"}:
     ui.run(host="0.0.0.0", port=1072)
