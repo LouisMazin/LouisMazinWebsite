9dfaa639527e1bb35323691e08418a59ec6dac92
Louis Mazin <mazinlouis79@gmail.com>
2024-10-15 14:05:28 +0200

    test

--- app.py
+++
@@ -1,8 +0,0 @@
-#little gradio website
-import gradio as gr
-
-def greet():
-    return "Hello !"
-
-iface = gr.Interface(fn=greet, outputs="text")
-iface.launch(share=False,server_name="0.0.0.0",server_port=1072)
